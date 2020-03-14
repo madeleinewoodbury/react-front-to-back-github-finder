0c3f8686c111a5c16d0ec4a56ea1b713c33c4bff
madeleinewoodbury <randine25@gmail.com>
2020-03-14 12:03:55 -0500

    Add Search component to query for github users by usernam

---
+++ src/components/users/Search.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Search extends Component {
+  state = {
+    text: ''
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit} className="form">
+        <input
+          type="text"
+          name="text"
+          value={this.state.text}
+          placeholder="Search Users..."
+          onChange={this.onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+    );
+  }
+}
+
+export default Search;
