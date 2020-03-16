ad1b5ca6ec6ed1037603d2aef60ef750f3558b46
madeleinewoodbury <randine25@gmail.com>
2020-03-16 15:16:33 -0500

    Update env vars

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
